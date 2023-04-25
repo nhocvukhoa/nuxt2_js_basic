45aeea5cd7bde6d111206283dc5ad54c271ac75d
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-04-25 16:46:21 +0700

    feature redirect using nuxt link

--- pages/decks/index.vue
+++ pages/decks/index.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
-    Your deck 
+    <h1>Your decks, please enter number of deck to show:</h1>
+    <input v-model="deckID" type="text" placeholder="Please enter deckID">
+    <button @click="showDeck">Go to deck</button>
   </div>
 </template>
 
-<script setup></script>
+<script>
+  export default {
+    data() {
+      return {
+        deckID: '1'
+      }
+    },
+    methods: {
+      showDeck() {
+        this.$router.push(`/decks/${this.deckID}`);
+      }
+    }
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="home">
-    Index
+  <div id="root">
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <nuxt-link to="/">Home</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/decks">Decks</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/profile">Profile</nuxt-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   </div>
 </template>
 
