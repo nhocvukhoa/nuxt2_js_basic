b5f373152f3c0c7e68cffbf79bd33ec6c857f7c9
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-04-26 16:40:25 +0700

    feature component styling

---
+++ components/Header/DefaultHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link to="/">Home</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/decks">Decks</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/profile">Profile</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/settings">Settings</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss">
+header {
+  background: #000;
+  height: 60px;
+  nav,
+  ul {
+    height: inherit;
+  }
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    li {
+      margin: 0 1.5rem;
+      a {
+        color: #fff;
+        text-decoration: none;
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,7 @@
 <template>
   <div id="root">
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <nuxt-link to="/">Home</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/decks">Decks</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/profile">Profile</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/settings">Settings</nuxt-link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <div>
+      <h1>Home page here</h1>
+    </div>
   </div>
 </template>
