f76141e4026f77f0f0d63f16245432053a30a71b
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-04-27 11:11:51 +0700

    feature import global css

---
+++ pages/decks/_id/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>This is deck ID: {{ $route.params.id }}</div>
+</template>
+
+<script>
+export default {
+  validate({ params }) {
+    // Must be a number
+    return /^\d+$/.test(params.id)
+  },
+}
+</script>
