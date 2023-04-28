48a1e5fa9d7616d64dc3494ec8d6e7d62cde11fb
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-04-28 14:10:25 +0700

    feature improved home page

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,18 @@
   <div>
     <default-header />
     <nuxt />
+    <default-footer />
   </div>
 </template>
 
 <script>
 import DefaultHeader from '@/components/Header/DefaultHeader.vue'
+import DefaultFooter from '@/components/Footer/DefaultFooter.vue'
+
 export default {
   components: {
     DefaultHeader,
+    DefaultFooter,
   },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,36 @@
 <template>
-  <div id="root">
-    <div>
-      <h1>Home page here</h1>
+  <div class="ct">
+    <div class="banner">
+      <div class="thumbnail-banner"></div>
+    </div>
+    <div class="popular-decks">
+      <h3 class="text_center my_3">3 Popular Decks For You</h3>
+      <div class="r">
+        <div class="c_4">
+          <div class="card">Deck 1</div>
+        </div>
+        <div class="c_4">
+          <div class="card">Deck 1</div>
+        </div>
+        <div class="c_4">
+          <div class="card">Deck 1</div>
+        </div>
+        <div class="c_4">
+          <div class="card">Deck 1</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.banner {
+  height: 220px;
+  .thumbnail-banner {
+    height: inherit;
+    background: url('https://previews.123rf.com/images/nataliias/nataliias1812/nataliias181200017/127109518-english-language-hand-drawn-doodles-and-lettering-education-banner-vector-illustration.jpg')
+      center center;
+    background-size: cover;
+  }
+}
+</style>
